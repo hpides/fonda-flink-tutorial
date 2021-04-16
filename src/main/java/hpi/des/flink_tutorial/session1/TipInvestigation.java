3f957b8e9efc2a9a3c2431aadf84a9fd50474a08
Pedro Silva <pedro.silva@hpi.de>
2021-04-16 12:03:06 +0200

    solutions for ex 2 and 3

--- src/main/java/hpi/des/flink_tutorial/session1/TipInvestigation.java
+++ src/main/java/hpi/des/flink_tutorial/session1/TipInvestigation.java
@@ -2,8 +2,14 @@ package hpi.des.flink_tutorial.session1;
 
 import hpi.des.flink_tutorial.util.InputFile;
 
+import hpi.des.flink_tutorial.util.TaxiRideTuple;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple7;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.time.LocalDateTime;
 
 public class TipInvestigation {
 
@@ -14,6 +20,9 @@ public class TipInvestigation {
         DataStreamSource<String> sourceStream = env.readTextFile(InputFile.getInputFilePath());
 
         sourceStream.flatMap(new TransformSourceStreamOperator())
+//                .map(new )
+
+//                .map(new Exercise1Operator())
                 // exercise 1
                 // exercise 2
                 // exercise 3
